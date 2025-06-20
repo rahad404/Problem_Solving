f7011803fa5673dc483535d7a4cd407e432bda40
rahad404 <rahad.0911@gmail.com>
2025-06-20 13:02:22 +0600

    fix 162.cpp

--- leetcode/162.cpp
+++ leetcode/162.cpp
@@ -2,19 +2,44 @@
 #include <vector>
 using namespace std;
 
-int findPeakElement(vector<int> nums){
-    int left=0, right=nums.size()-1, mid=left+(right-left)/2;
-    while(left<=right){
-        if(mid>0 && nums[mid]<nums[mid-1]){
-            right = mid-1;
+// int findPeakElement(vector<int> nums){
+//     int left=0, right=nums.size()-1, mid=left+(right-left)/2;
+//     while(left<=right){
+//         if(mid>0 && nums[mid]<nums[mid-1]){
+//             right = mid-1;
+//         }
+//         else if(mid<nums.size()-1 && nums[mid]<nums[mid+1]){
+//             left = mid+1;
+//         }
+//         mid = left+(right-left)/2;
+//     }
+//     return -1;
+// }
+
+int findPeakElement(vector<int>& nums) {
+    int left = 0, right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        // Handle edge cases safely
+        bool leftOk = (mid == 0) || (nums[mid] > nums[mid - 1]);
+        bool rightOk = (mid == nums.size() - 1) || (nums[mid] > nums[mid + 1]);
+
+        if (leftOk && rightOk) {
+            return mid; // peak found
         }
-        else if(mid<nums.size()-1 && nums[mid]<nums[mid+1]){
-            left = mid+1;
+        else if (mid < nums.size() - 1 && nums[mid] < nums[mid + 1]) {
+            left = mid + 1; // move to right side
         }
-        mid = left+(right-left)/2;
+        else {
+            right = mid - 1; // move to left side
+        }
     }
-    return -1;
+
+    return -1; // technically unreachable since a peak is always guaranteed
 }
+
 
 int main(){
     int n;
