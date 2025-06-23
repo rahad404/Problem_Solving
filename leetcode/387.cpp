3ad8368cc34fee5d384a947ed2a67332aec46bf7
rahad404 <rahad.0911@gmail.com>
2025-06-23 18:52:43 +0600

    solve 387.cpp with queue

--- leetcode/387.cpp
+++ leetcode/387.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include<unordered_map>
 #include<cstring>
 #include<queue>
 using namespace std;
 
+int firstUniqChar(string s) {
+    unordered_map<char, int>count;
+    queue<pair<char, int>> q;
+
+    for(int i=0;i<s.length();i++){
+        count[s[i]]++;
+        q.push({s[i], i});
+
+        while(!q.empty() && count[q.front().first]>1){
+            q.pop();
+        }
+    }
+    
+    if(!q.empty()) return q.front().second;
+    return -1;
+}
+
+/**
 int firstUniqChar(string s) {
     int count[26];
     memset(count, 0, sizeof(count));
@@ -17,6 +36,7 @@ int firstUniqChar(string s) {
     }
     return -1;
 }
+**/
 
 int main(){
     string s;
