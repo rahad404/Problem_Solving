22ac5712970f041f400b430967e399679ede488e
rahad404 <rahad.0911@gmail.com>
2025-06-27 20:02:01 +0600

    solve leetcode 1700

---
+++ leetcode/1700.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<vector>
+using namespace std;
+
+int countStudents(vector<int>& students, vector<int>& sandwiches){
+    queue<int> q;
+    for(auto x:students)q.push(x);
+    stack<int> s;
+    for(int i=sandwiches.size()-1;i>=0;i--)s.push(sandwiches[i]);
+
+    int check = 0;
+    while(!s.empty()){
+        if(q.front()==s.top()){
+            q.pop();
+            s.pop();
+            check = 0;
+        }
+        else{
+            int temp=q.front();
+            q.pop();
+            q.push(temp);
+            check++;
+            if(check==s.size()) break;
+        }
+    }
+    int count=0;
+    while (!q.empty()){
+        count++;
+        q.pop();
+    }
+    return count;
+}
+
+
+int main(){
+
+}
