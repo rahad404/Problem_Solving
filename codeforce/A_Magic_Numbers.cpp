287b5300007fa612e56dd9fc9e4ea92a93e67af9
rahad404 <rahad.0911@gmail.com>
2025-11-24 23:44:59 +0600

    magic number is done

--- codeforce/A_Magic_Numbers.cpp
+++ codeforce/A_Magic_Numbers.cpp
@@ -5,37 +5,18 @@ int main(){
     string s;
     cin >>s;
 
-    bool yes=0;
-
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='1'&&(s[i+1]=='1'||
-            (s[i+1]=='1'&&s[i+2]=='4')||
-            (s[i+1]=='1'&&s[i+2]=='4'&&s[i+2]=='4'))){
-            yes=1;
-            break;
+    if((s.find("444")!=s.npos)||s[0]=='4'){
+        cout<<"NO" <<endl;
+    }
+    else{
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='4' && s[i]!='1'){
+                cout<<"NO" <<endl;
+                return 0;
+            }
         }
-        else if((s[i]=='1'&&s[i+1]=='4')&&(s[i+2]=='1'||
-            (s[i+2]=='1'&&s[i+3]=='4')||
-            (s[i+2]=='1'&&s[i+3]=='4'&&s[i+4]=='4'))){
-            yes=1;
-            break;               
-        }
-        else if((s[i]=='1'&&s[i+1]=='4'&&s[i+2]=='4')&&(s[i+3]=='1'||
-            (s[i+3]=='1'&&s[i+4]=='4')||
-            (s[i+3]=='1'&&s[i+4]=='4'&&s[i+5]=='4'))){
-            yes=1;
-            break;
-        }
+        cout<<"YES" <<endl;
     }
 
-    if((s.size()==1 && s[0]=='1')||
-        (s.size()==2 && s[0]=='1' && s[1]=='4')||
-        (s.size()==3 && s[0]=='1' && s[1]=='4' && s[2]=='4')){
-        yes=1;
-    }
-
-    if(yes)cout<<"YES" <<endl;
-    else cout<<"NO" <<endl;
-    
     return 0;
 }
